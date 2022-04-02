444f640c64118ec88e983b2775dd29f2ab0360eb
Douglas Monteiro <douglasm.dev@gmail.com>
2022-04-02 15:52:12 -0300

    Primeiro commit dms player

---
+++ assets/scripts/main.js
@@ -0,0 +1,168 @@
+let sound = document.querySelector("#sound");
+let title = document.querySelector("#title");
+let artist = document.querySelector("#artist");
+let logo = document.querySelector("#logo");
+let btn_play = document.querySelector("#btn-play");
+let btn_pause = document.querySelector("#btn-pause");
+let bar_progress = document.querySelector("#bar_progress");
+let volume_bar = document.querySelector("#volume_bar");
+let end_music = document.querySelector(".end");
+let elapsed_time = document.querySelector(".start");
+let vol = document.querySelector("#vol");
+let muted = document.querySelector("#muted");
+let player_music = document.querySelector(".player_music");
+let playlist = document.querySelector(".playlist");
+let list_music = document.querySelector("#list_music");
+let repeat_click = false;
+var i = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  sound.src = list_songs[i].src_audio;
+  list_songs.forEach((musica) => {
+    list_music.innerHTML += `<div class="pl_music">
+                            <i class="fa-solid fa-play btn_tocar"></i>
+                            ${musica.name_music} - ${musica.artist}
+                            </div> `;
+    add_click_playlist();
+  });
+});
+
+function add_click_playlist() {
+  let botaoPlay = document.querySelectorAll(".btn_tocar");
+  for (let index in botaoPlay) {
+    botaoPlay[index].onclick = () => {
+      i = Number(index);
+      att_dados();
+      att_playlist();
+    };
+  }
+}
+
+function att_playlist() {
+  playlist.style.display = "none";
+  player_music.style.display = "flex";
+  btn_play.style.display = "none";
+  btn_pause.style.display = "flex";
+}
+
+function show_playlist() {
+  playlist.style.display = "flex";
+  player_music.style.display = "none";
+}
+
+function close_playlist() {
+  playlist.style.display = "none";
+  player_music.style.display = "flex";
+}
+
+function play() {
+  logo.src = list_songs[i].src_image;
+  title.innerHTML = list_songs[i].name_music;
+  artist.innerHTML = list_songs[i].artist;
+  sound.play();
+  setTimeout(duration_song, 100);
+  btn_play.style.display = "none";
+  btn_pause.style.display = "flex";
+}
+
+function pause() {
+  sound.pause();
+  btn_pause.style.display = "none";
+  btn_play.style.display = "flex";
+}
+
+function next() {
+  i += 1;
+  if (i >= list_songs.length) {
+    i = 0;
+  }
+  att_dados();
+}
+
+function back() {
+  i -= 1;
+  if (i < 0) {
+    i = list_songs.length - 1;
+  }
+  att_dados();
+}
+
+function att_dados() {
+  logo.src = list_songs[i].src_image;
+  title.innerHTML = list_songs[i].name_music;
+  sound.src = list_songs[i].src_audio;
+  artist.innerHTML = list_songs[i].artist;
+  btn_play.style.display = "none";
+  btn_pause.style.display = "flex";
+  sound.play();
+  setTimeout(duration_song, 100);
+}
+
+function duration_song() {
+  end_music.innerHTML = format_timer(Math.floor(sound.duration));
+}
+
+function format_timer(time) {
+  let min = Math.floor(time / 60);
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  let sec = Math.floor(time % 60);
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+  return `${min}:${sec}`;
+}
+
+setInterval(() => {
+  bar_progress.value = sound.currentTime;
+  if (bar_progress.value <= 3) {
+    bar_progress.setAttribute("max", sound.duration);
+  }
+  if (sound.currentTime == sound.duration && repeat_click == true) {
+    sound.play();
+  }
+  if (sound.currentTime == sound.duration) {
+    next();
+  }
+  elapsed_time.innerHTML = format_timer(sound.currentTime);
+}, 250);
+
+bar_progress.addEventListener("input", () => {
+  sound.currentTime = bar_progress.value;
+});
+
+vol.addEventListener("click", () => {
+  sound.volume = 0;
+  muted.style.display = "inline";
+  vol.style.display = "none";
+});
+
+muted.addEventListener("click", () => {
+  volume_bar.value = 100;
+  sound.volume = 1;
+  vol.style.display = "inline";
+  muted.style.display = "none";
+});
+
+function volume() {
+  sound.volume = volume_bar.value / 100;
+  if (sound.volume == 0) {
+    muted.style.display = "inline";
+    vol.style.display = "none";
+  } else {
+    vol.style.display = "inline";
+    muted.style.display = "none";
+  }
+}
+
+function repeat() {
+  let btn_repeat = document.querySelector("#btn_repeat");
+  if (repeat_click == false) {
+    repeat_click = true;
+    btn_repeat.style.color = "gold";
+  } else {
+    repeat_click = false;
+    btn_repeat.style.color = "white";
+  }
+}
---
+++ assets/scripts/sounds.js
@@ -0,0 +1,63 @@
+let list_songs = [
+  {
+    name_music: "Lose Yourself",
+    artist: "EMINEM",
+    src_audio: "./assets/musics/Lose_Yourself.mp3",
+    src_image: "./assets/logos/lose_yourself.jpeg",
+  },
+  {
+    name_music: "Blinding Lights",
+    artist: "The WEEKND",
+    src_audio: "./assets/musics/Blinding_Lights.mp3",
+    src_image: "./assets/logos/blinding_lights.jpeg",
+  },
+  {
+    name_music: "Congratulations",
+    artist: "POST MALONE",
+    src_audio: "./assets/musics/Congratulations.mp3",
+    src_image: "./assets/logos/congratulations.jpeg",
+  },
+  {
+    name_music: "G.D.F.R",
+    artist: "FLO RIDA",
+    src_audio: "./assets/musics/GDFR.mp3",
+    src_image: "./assets/logos/gdfr.jpeg",
+  },
+  {
+    name_music: "Gods Plan",
+    artist: "DRAKE",
+    src_audio: "./assets/musics/Gods_Plan.mp3",
+    src_image: "./assets/logos/gods_plan.jpeg",
+  },
+  {
+    name_music: "Good Feeling",
+    artist: "FLO RIDA",
+    src_audio: "./assets/musics/Good_Feeling.mp3",
+    src_image: "./assets/logos/good_feeling.jpeg",
+  },
+  {
+    name_music: "Lonely",
+    artist: "AKON",
+    src_audio: "./assets/musics/Lonely.mp3",
+    src_image: "./assets/logos/lonely.jpeg",
+  },
+
+  {
+    name_music: "Gangstas Paradise",
+    artist: "COOLIO",
+    src_audio: "./assets/musics/Gangstas_Paradise.mp3",
+    src_image: "./assets/logos/gangster.jpeg",
+  },
+  {
+    name_music: "The Hills",
+    artist: "The WEEKND",
+    src_audio: "./assets/musics/The_Hills.mp3",
+    src_image: "./assets/logos/the_hills.jpeg",
+  },
+  {
+    name_music: "Whistle",
+    artist: "FLO RIDA",
+    src_audio: "./assets/musics/Whistle.mp3",
+    src_image: "./assets/logos/whistle.jpeg",
+  },
+];
